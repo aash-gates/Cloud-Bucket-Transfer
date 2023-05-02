9d567e72a41c3c20d96d36a548ecc35cfc5f0a52
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-05-02 16:56:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const previewContainer = document.querySelector("#previewContainer");
 const uploadButton = document.querySelector("#uploadButton");
 
 fileInput.addEventListener("change", () => {
+  const files = fileInput.files;
