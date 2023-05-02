cb173d0233cf59a11fd353860f985eb4afcc43b4
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-05-02 17:07:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,4 @@ uploadButton.addEventListener("click", () => {
       accessKeyId: "YOUR_ACCESS_KEY_ID",
       secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
       region: "YOUR_BUCKET_REGION"
+    });
